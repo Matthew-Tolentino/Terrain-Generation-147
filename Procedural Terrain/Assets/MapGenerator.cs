085196593735b99f1e21f6a5fa940b1411c62860
Henry Nguyen <63266970+cobaln@users.noreply.github.com>
2021-03-04 14:15:41 -0800

    Added Colors and Mesh

--- Procedural Terrain/Assets/MapGenerator.cs
+++ Procedural Terrain/Assets/MapGenerator.cs
@@ -4,12 +4,16 @@ using UnityEngine;
 
 public class MapGenerator : MonoBehaviour
 {
+
+    public enum DrawMode { NoiseMap, ColourMap, Mesh };
+    public DrawMode drawMode;
+
     public int mapWidth;
     public int mapHeight;
     public float noiseScale;
 
     public int octaves;
-    [Range(0,1)]
+    [Range(0, 1)]
     public float persistance;
     public float lacunarity;
 
@@ -18,12 +22,42 @@ public class MapGenerator : MonoBehaviour
 
     public bool autoUpdate;
 
+    public TerrainType[] regions;
+
     public void GenerateMap()
     {
         float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, seed, noiseScale, octaves, persistance, lacunarity, offset);
 
+        Color[] colourMap = new Color[mapWidth * mapHeight];
+        for (int y = 0; y < mapHeight; y++)
+        {
+            for (int x = 0; x < mapWidth; x++)
+            {
+                float currentHeight = noiseMap[x, y];
+                for (int i = 0; i < regions.Length; i++)
+                {
+                    if (currentHeight <= regions[i].height)
+                    {
+                        colourMap[y * mapWidth + x] = regions[i].colour;
+                        break;
+                    }
+                }
+            }
+        }
+
         MapDisplay display = FindObjectOfType<MapDisplay>();
-        display.DrawNoiseMap(noiseMap);
+        if (drawMode == DrawMode.NoiseMap)
+        {
+            display.DrawTexture(TextureGenerator.TextureFromHeightMap(noiseMap));
+        }
+        else if (drawMode == DrawMode.ColourMap)
+        {
+            display.DrawTexture(TextureGenerator.TextureFromColourMap(colourMap, mapWidth, mapHeight));
+        }
+        else if (drawMode == DrawMode.Mesh)
+        {
+            display.DrawMesh(MeshGenerator.GenerateTerrainMesh(noiseMap), TextureGenerator.TextureFromColourMap(colourMap, mapWidth, mapHeight));
+        }
     }
 
     void OnValidate()
@@ -45,4 +79,12 @@ public class MapGenerator : MonoBehaviour
             octaves = 0;
         }
     }
+}
+
+[System.Serializable]
+public struct TerrainType
+{
+    public string name;
+    public float height;
+    public Color colour;
 }
--- Procedural Terrain/Assets/MeshGenerator.cs
+++ Procedural Terrain/Assets/MeshGenerator.cs
@@ -2,81 +2,73 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(MeshFilter))]
-public class MeshGenerator : MonoBehaviour
+public static class MeshGenerator
 {
-    Mesh mesh;
 
-    Vector3[] vertices;
-    int[] triangles;
-
-    public int xSize = 20;
-    public int zSize = 20;
-    // Start is called before the first frame update
-    void Start()
+    public static MeshData GenerateTerrainMesh(float[,] heightMap)
     {
-        mesh = new Mesh();
-        GetComponent<MeshFilter>().mesh = mesh;
+        int width = heightMap.GetLength(0);
+        int height = heightMap.GetLength(1);
+        float topLeftX = (width - 1) / -2f;
+        float topLeftZ = (height - 1) / 2f;
 
-        CreateShape();
-        UpdateMesh();
-    }
+        MeshData meshData = new MeshData(width, height);
+        int vertexIndex = 0;
 
-    void CreateShape()
-    {
-        vertices = new Vector3[(xSize + 1) * (zSize + 1)];
-
-        for (int i = 0, z = 0; z <= zSize; z++)
+        for (int y = 0; y < height; y++)
         {
-            for (int x = 0; x <= xSize; x++)
+            for (int x = 0; x < width; x++)
             {
-                float y = Mathf.PerlinNoise(x * .3f, z * .3f) * 10f;
 
-                vertices[i] = new Vector3(x, y, z);
-                i++;
+                meshData.vertices[vertexIndex] = new Vector3(topLeftX + x, heightMap[x, y], topLeftZ - y);
+                meshData.uvs[vertexIndex] = new Vector2(x / (float)width, y / (float)height);
+
+                if (x < width - 1 && y < height - 1)
+                {
+                    meshData.AddTriangle(vertexIndex, vertexIndex + width + 1, vertexIndex + width);
+                    meshData.AddTriangle(vertexIndex + width + 1, vertexIndex, vertexIndex + 1);
+                }
+
+                vertexIndex++;
             }
         }
 
-        triangles = new int[xSize * zSize * 6];
+        return meshData;
 
-        int vert = 0;
-        int tris = 0;
-        for (int z = 0; z < zSize; z++)
-        {
-            for (int x = 0; x < xSize; x++)
-            {
-                triangles[tris + 0] = vert + 0;
-                triangles[tris + 1] = vert + xSize + 1;
-                triangles[tris + 2] = vert + 1;
-                triangles[tris + 3] = vert + 1;
-                triangles[tris + 4] = vert + xSize + 1;
-                triangles[tris + 5] = vert + xSize + 2;
+    }
+}
 
-                vert++;
-                tris += 6;
-            }
-            vert++;
-        }
+public class MeshData
+{
+    public Vector3[] vertices;
+    public int[] triangles;
+    public Vector2[] uvs;
+
+    int triangleIndex;
+
+    public MeshData(int meshWidth, int meshHeight)
+    {
+        vertices = new Vector3[meshWidth * meshHeight];
+        uvs = new Vector2[meshWidth * meshHeight];
+        triangles = new int[(meshWidth - 1) * (meshHeight - 1) * 6];
     }
 
-    void UpdateMesh()
+    public void AddTriangle(int a, int b, int c)
     {
-        mesh.Clear();
+        triangles[triangleIndex] = a;
+        triangles[triangleIndex + 1] = b;
+        triangles[triangleIndex + 2] = c;
+        triangleIndex += 3;
+    }
 
+    public Mesh CreateMesh()
+    {
+        Mesh mesh = new Mesh();
         mesh.vertices = vertices;
         mesh.triangles = triangles;
-
+        mesh.uv = uvs;
         mesh.RecalculateNormals();
+        return mesh;
     }
 
-    private void OnDrawGizmos()
-    {
-        if (vertices == null)
-            return;
-
-        for (int i = 0; i < vertices.Length; i++)
-        {
-            Gizmos.DrawSphere(vertices[i], .1f);
-        }
-    }
 }
