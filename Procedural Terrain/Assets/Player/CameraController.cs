45372561908408e5f90ceb654d3c06928d4a02b6
Matthew <lemtolen@ucsc.edu>
2021-03-16 17:28:42 -0700

    Added in player movement

---
+++ Procedural Terrain/Assets/Player/CameraController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform player;
+    public float mouseSense = 100f;
+    public float maxY = 90f;
+    public float minY = -90f;
+
+    float yRotation = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSense * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSense * Time.deltaTime;
+
+        yRotation -= mouseY;
+        yRotation = Mathf.Clamp(yRotation, minY, maxY);
+
+        transform.localRotation = Quaternion.Euler(yRotation, 0f, 0f);
+        player.Rotate(Vector3.up * mouseX);
+    }
+}
---
+++ Procedural Terrain/Assets/Player/PlayerMovement.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController player;
+
+    public float speed = 5f;
+    public float jumpHeight = 3f;
+
+    [Header("Ground Checks")]
+    public float groundDistance = .25f;
+    public LayerMask groundMask;
+
+    public float gravity = -9.81f;
+    bool isGrounded;
+    Vector3 velocity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        // Gravity Handeling
+        Vector3 checkPos = new Vector3(transform.position.x, transform.position.y - 1.6f, transform.position.z);
+        if (Physics.CheckSphere(checkPos, groundDistance, groundMask))
+        {
+            isGrounded = true;
+        }
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        velocity.y += gravity + Time.deltaTime;
+        player.Move(velocity * Time.deltaTime);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            player.Move(transform.forward * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            player.Move(-transform.forward * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            player.Move(-transform.right * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            player.Move(transform.right * speed * Time.deltaTime);
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(new Vector3(transform.position.x, transform.position.y - 1f, transform.position.z), .25f);
+    }
+}
